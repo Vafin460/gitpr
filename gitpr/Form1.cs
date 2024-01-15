b4b8441811e62636ce07da4ab67ec415a5143027
vadim vafin <vafinv077@gmail.com>
2024-01-15 14:20:46 +0500

    сделал так, чтобы в углах квадрата появились квадраты которые меньше оригинала в 4 раза(в 2 раза меньше ширина), но главный квадрат куда-то пропал

--- gitpr/Form1.cs
+++ gitpr/Form1.cs
@@ -18,27 +18,54 @@ namespace gitpr
         }
         // width - ширина - x
         // height - высота - y
+        public int iter = 1;
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            
-        }
 
+        }
+        
         private void button1_Click(object sender, EventArgs e)
         {
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Brush sb = new SolidBrush(Color.FromArgb(100, 100, 100, 100));
+
             float w = pictureBox1.Width;
             float h = pictureBox1.Height;
             float x, y;
-            g.Clear(Color.White);
-            Brush sb = new SolidBrush(Color.Black);
 
-            float drw = w / 7;
-            float drh = w / 7;
-            x = w / 2 - drw / 2;
-            y = h / 2 - drh /2;
+            
+
+
+           
+            float drw = w / (7);
+            float drh = w / (7);
+            x = (w / 2 - drw / 2);
+            y = (h / 2 - drh / 2);
             g.FillRectangle(sb, x, y, drw, drh);
+            makeFractal(x, y, drw, drh, 4);
+            
+        }
+        public void makeFractal(float x, float y, float drw, float drh, int maxIter)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Brush sb = new SolidBrush(Color.FromArgb(100, 100, 100, 100));
 
+            if (iter == maxIter)
+            {
+                return;
+            }
+            iter++;
+
+
+            g.FillRectangle(sb, x - (drw / 2), y - (drh / 2), drw / 2, drh / 2); // верхний левый 
+            g.FillRectangle(sb, x + drw      , y - (drh / 2), drw / 2, drh / 2); // вехиний правый 
+            g.FillRectangle(sb, x - (drw / 2), y + (drh    ), drw / 2, drh / 2); // нижний  левый
+            g.FillRectangle(sb, x + (drw    ), y + (drh    ), drw / 2, drh / 2); // нижний  правый
+
+
+            
         }
     }
 }
