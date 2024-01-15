03b597f6e1e42114c65237dc354a988010bd0894
vadim vafin <vafinv077@gmail.com>
2024-01-15 11:04:50 +0500

    нарисовал квадрат

--- gitpr/Form1.cs
+++ gitpr/Form1.cs
@@ -16,5 +16,23 @@ namespace gitpr
         {
             InitializeComponent();
         }
+        // width - ширина - x
+        // height - высота - y
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            float w = pictureBox1.Width;
+            float h = pictureBox1.Height;
+            g.Clear(Color.White);
+            Brush pn = new SolidBrush(Color.Black);
+            g.FillRectangle(pn,
+                w / 2- w / 10 / 2, 
+                h / 2, 
+                w / 10, 
+                w / 10
+                );
+
+        }
     }
 }
