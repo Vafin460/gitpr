543621e2eb4c5d04d50e4bdc4768feacd396d77c
vadim vafin <vafinv077@gmail.com>
2024-01-15 11:39:14 +0500

    переделал рисование квадрата

--- gitpr/Form1.cs
+++ gitpr/Form1.cs
@@ -20,18 +20,24 @@ namespace gitpr
         // height - высота - y
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             Graphics g = Graphics.FromImage(pictureBox1.Image);
             float w = pictureBox1.Width;
             float h = pictureBox1.Height;
+            float x, y;
             g.Clear(Color.White);
-            Brush pn = new SolidBrush(Color.Black);
-            g.FillRectangle(pn,
-                w / 2- w / 10 / 2, 
-                h / 2, 
-                w / 10, 
-                w / 10
-                );
+            Brush sb = new SolidBrush(Color.Black);
+
+            float drw = w / 7;
+            float drh = w / 7;
+            x = w / 2 - drw / 2;
+            y = h / 2 - drh /2;
+            g.FillRectangle(sb, x, y, drw, drh);
 
         }
     }
